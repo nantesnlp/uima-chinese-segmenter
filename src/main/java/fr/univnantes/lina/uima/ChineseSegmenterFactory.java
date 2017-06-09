5f661d034a60712c76e3a6a2ac8a90f41432636e
Damien Cram <damien.cram@univ-nantes.fr>
2017-06-09 13:16:03 +0200

    Moved resources to src/main/resources and added factory

---
+++ src/main/java/fr/univnantes/lina/uima/ChineseSegmenterFactory.java
@@ -0,0 +1,38 @@
+package fr.univnantes.lina.uima;
+
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.ExternalResourceFactory;
+
+import fr.univnantes.lina.uima.engines.ChineseSegmenter;
+import fr.univnantes.lina.uima.models.ChineseSegmentResource;
+
+public class ChineseSegmenterFactory {
+	
+	public static AnalysisEngineDescription createChineseSegmenterAE(String annotationType) {
+		try {
+			AnalysisEngineDescription ae = AnalysisEngineFactory.createEngineDescription(
+					ChineseSegmenter.class,
+					ChineseSegmenter.ANNOTATION_TYPE, annotationType
+				);
+			ExternalResourceFactory.createDependencyAndBind(
+					ae,
+					ChineseSegmenter.CHINESE_WORD_SEGMENTS, 
+					ChineseSegmentResource.class, 
+					ChineseSegmenterResourceHelper.getChineseWordSegments());
+			ExternalResourceFactory.createDependencyAndBind(
+					ae,
+					ChineseSegmenter.CHINESE_FOREIGN_NAME_SEGMENTS, 
+					ChineseSegmentResource.class, 
+					ChineseSegmenterResourceHelper.getForeignNameSegments());
+			ExternalResourceFactory.createDependencyAndBind(
+					ae,
+					ChineseSegmenter.CHINESE_NUMBER_SEGMENTS, 
+					ChineseSegmentResource.class, 
+					ChineseSegmenterResourceHelper.getNumberSegments());
+			return ae;
+		} catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
